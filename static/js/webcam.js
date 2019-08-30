c256bf2e456fb4f056b6b7f1211d5dd8dd25dd28
zx <zhx281@gmail.com>
2019-08-29 23:14:01 -0600

    update webcam.js

--- static/js/webcam.js
+++ static/js/webcam.js
@@ -14,6 +14,7 @@ async function setupWebcam() {
                 },
                 error => reject());
         } else {
+            $("#webcam").text('No webcam detected!')
             reject();
         }
     });
